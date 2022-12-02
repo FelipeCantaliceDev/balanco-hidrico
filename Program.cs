4cde17d5ae6f2d37118a144fff7ad0e67aa80e37
FelipeCantaliceDev <116805996+FelipeCantaliceDev@users.noreply.github.com>
2022-12-02 07:17:13 -0400

    IMPL: Aumento no nivel de detalhes para o usuario visualizar

--- Program.cs
+++ Program.cs
@@ -3,13 +3,14 @@
 namespace Main
 {
     public class Program
-    {        
-    
+    {
+
         public static void Main()
         {
-
+            // Cria um sistema de monitoramento
             var system = new Monitoramento();
 
+            // Liquidos administrados no periodo de 24 horas
             var lista = new List<Liquido>{
                 new EntradaLiquido(10.0),
                 new SaidaLiquido(9.0),
@@ -24,13 +25,28 @@ namespace Main
                 new EntradaLiquido(9.1),
             };
 
+            // Insere todos os elementos
             foreach (var item in lista)
             {
                 system.incluir(item);
             }
 
+
+            // Verifica o balanço
             Console.WriteLine(system.total);
+            // Verifica o estado do balanço
             Console.WriteLine(system.estado());
+            // Verifica todos os liquidos adminstrados
+            foreach (var liquido in system.administrados())
+            {
+                Console.WriteLine(liquido.dosagem);
+            }
+
+            // Verifica todos os liquidos perdidos
+            foreach (var liquido in system.perdidos())
+            {
+                Console.WriteLine(liquido.dosagem);
+            }
 
         }
     }
